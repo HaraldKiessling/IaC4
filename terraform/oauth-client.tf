08afa574a6a8c11e9916531f491fc4dd0d6f5243
Nova (via Harald Kiessling) <harald.kiessling@web.de>
2026-07-30 19:27:35 +0000

    fix(terraform): OAuth-Client darf auch tag:ia3 Keys erzeugen

--- terraform/oauth-client.tf
+++ terraform/oauth-client.tf
@@ -1,5 +1,5 @@
 resource "tailscale_oauth_client" "ci_client_ia4" {
   description = "IaC4 CI OAuth Client ${formatdate("YYYY-MM-DD", timestamp())}"
   scopes      = var.oauth_client_scopes
-  tags        = ["tag:ci"]
+  tags        = ["tag:ci", "tag:ia3"]
 }
