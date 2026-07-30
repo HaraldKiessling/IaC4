4502591b8ebbfcb9c248d9564a5bc95ecb288d59
Nova (via Harald Kiessling) <harald.kiessling@web.de>
2026-07-30 17:11:21 +0000

    fix(terraform): use only tag:ci for OAuth client (tag:ia4 not in ACL yet)

--- terraform/oauth-client.tf
+++ terraform/oauth-client.tf
@@ -1,5 +1,5 @@
 resource "tailscale_oauth_client" "ci_client_ia4" {
   description = "IaC4 CI OAuth Client Terraform managed"
   scopes      = var.oauth_client_scopes
-  tags        = ["tag:ci", "tag:ia4"]
+  tags        = ["tag:ci"]
 }
